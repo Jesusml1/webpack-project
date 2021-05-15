2aeea7e59c4a6799e9c2b8e73f6b4214139d3eaa
jesus <jesus_lopez1997@hotmail.com>
2021-05-14 21:33:42 -0400

    React setup

---
+++ src/components/Recepies.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+
+const clothes = {
+  shoes: 2,
+  shirts: 4,
+  pants: 1,
+};
+
+const things = {
+  ...clothes,
+  table: 4,
+  glass: 7,
+};
+
+const Recipes = () => {
+  const [recipe, setRecipe] = useState([]);
+
+  return (
+    <div>
+      <h3>Current Recipes:</h3>
+      <button onClick={() => setRecipe(clothes)}>Clothes</button>
+      <button onClick={() => setRecipe(things)}>Things</button>
+      <ul>
+        {Object.keys(recipe).map((material) => (
+          <li key={material}>
+            {material}: {recipe[material]}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Recipes;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,4 @@
-import "./styles/index.scss";
+import { render } from "react-dom";
+import App from "./components/App";
 
-const clothes = {
-  shoes: 2,
-  shirts: 4,
-  pants: 1,
-};
-
-const things = {
-  ...clothes,
-  table: 4,
-  glass: 7,
-};
-
-console.log(things);
+render(<App />, document.getElementById("root"));
